341240c5b91f06082c7c34484dad92958d4e68c9
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 23:01:08 +0300

    add implementation of longest sequence task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+function bubbleSort(array) {
+  var done = false;
+  while (!done) {
+    done = true;
+    for (var i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        var tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array;
+}
+
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let sortedArr = bubbleSort(array);
+  let output = [];
+  let i = 0;
+  let counter = 0;
+  while (i < sortedArr.length) {
+    if (sortedArr[i+1] - sortedArr[i] === 1) {
+      counter++;
+    } else { 
+      output.push(counter);
+      counter = 0;
+    }
+    i++;
+  }
+  return Math.max.apply(null, output) === 0 ? 0 : Math.max.apply(null, output) + 1;
 }
